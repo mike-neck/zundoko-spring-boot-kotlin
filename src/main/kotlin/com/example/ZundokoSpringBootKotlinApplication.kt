8a3e323f82564a5375f5ce42bf631e1d566e0cbd
mike_neck <jkrt3333@gmail.com>
2017-04-10 23:57:40 +0900

    add files

---
+++ src/main/kotlin/com/example/ZundokoSpringBootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ZundokoSpringBootKotlinApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ZundokoSpringBootKotlinApplication::class.java, *args)
+}
